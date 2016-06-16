ecfae4382844bf29a26a6089f41197055db0877a
Bruno <burunoshiro@gmail.com>
2016-06-16 17:42:14 -0300

    Commit inicial.

---
+++ livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -0,0 +1,35 @@
+package br.com.caelum.livraria.bean;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Autor;
+import br.com.caelum.livraria.modelo.Livro;
+
+@ManagedBean
+public class LivroBean {
+
+	private Livro livro = new Livro();
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+
+		if (livro.getAutores().isEmpty()) {
+			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+		}
+
+		new DAO<Livro>(Livro.class).adiciona(this.livro);
+	}
+
+	public List<Autor> getAutores(){
+		
+		return new DAO<Autor>(Autor.class).listaTodos();
+		
+	}
+}
