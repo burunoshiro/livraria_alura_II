699bfc4778b824289c959dfada49c7629e1a6974
Bruno <burunoshiro@gmail.com>
2016-06-21 17:58:36 -0300

    Início aula 10

--- livraria/src/br/com/caelum/livraria/bean/AutorBean.java
+++ livraria/src/br/com/caelum/livraria/bean/AutorBean.java
@@ -1,22 +1,66 @@
 package br.com.caelum.livraria.bean;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 
 @ManagedBean
-public class AutorBean {
+@ViewScoped
+public class AutorBean implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Autor autor = new Autor();
+	private Integer autorId;
+	
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
 
 	public Autor getAutor() {
 		return autor;
 	}
 
+	public void carregarAutorPeloId() {
+		this.autor = new DAO<Autor>(Autor.class).buscaPorId(autorId);
+	}
+	
 	public void gravar() {
 		System.out.println("Gravando autor " + this.autor.getNome());
 
-		new DAO<Autor>(Autor.class).adiciona(this.autor);
+		if(this.autor.getId() != null && this.autor.getId().intValue() > 0) {
+			new DAO<Autor>(Autor.class).atualiza(this.autor);
+		}
+		else {
+			new DAO<Autor>(Autor.class).adiciona(this.autor);
+		}
+	}
+	
+	public void remover(Autor autor){
+		new DAO<Autor>(Autor.class).remove(autor);
+	}
+	
+	public List<Autor> getListaAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public void alterar(Autor autor) {
+		System.out.println("Alterando autor");
+		this.autor = autor;
 	}
 }
